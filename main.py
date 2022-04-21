89fc98667b2f532908b38b8aa6e3e0937b65ff95
MurathaAttaTEN <attamuratha@gmail.com>
2022-04-21 15:48:28 +0300

    conactenation

---
+++ main.py
@@ -0,0 +1,17 @@
+msg="you're cute"
+msg2="hmu"
+full_message=msg + "..."+ msg2
+print(full_message)
+print(msg + " ..."+ msg2)
+print(msg,msg2)
+print("Hey" " " "There!")
+psg=("This is a long string ...."
+"continue")
+print(psg)
+poem = """This is all combined
+as one happy string...
+what was that sound?
+it was a doorbell,ring.
+When i see you,my heart sings
+here please.Take this diamond ring"""
+print(poem)
